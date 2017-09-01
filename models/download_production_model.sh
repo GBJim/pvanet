1162393e3d1631545f77bc2f0ca189a876625dc4
GBJim <ccha97u@gmail.com>
2017-09-01 07:01:56 +0000

    Fix bug of downloading script not output to correct path

--- models/download_production_model.sh
+++ models/download_production_model.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-wget ftp://172.16.22.51/PVA-RC/rc1_iter_200000.caffemodel
+wget ftp://172.16.22.51/PVA-RC/rc1_iter_200000.caffemodel -O  /root/pva-faster-rcnn/models/rc1_iter_200000.caffemodel
 
