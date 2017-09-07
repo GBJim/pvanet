4b7f6ee7f8d83d0c3b25a17e009d1346cf84d904
IY0057 <chunhao.chang@ironyun.com>
2017-09-07 02:57:27 +0000

    Modify the download address of RC model

--- models/download_production_model.sh
+++ models/download_production_model.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-wget ftp://172.16.22.51/PVA-RC/rc1_iter_200000.caffemodel -O  /root/pva-faster-rcnn/models/rc1_iter_200000.caffemodel
+wget http://172.16.15.108/DL/PVA-RC/rc1_iter_200000.caffemodel -O  /root/pva-faster-rcnn/models/rc1_iter_200000.caffemodel
 
