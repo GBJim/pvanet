5ca50b3e5a60a472347857a30f3c995ed4e3320b
Jim <ccha97u@gmail.com>
2018-02-21 19:27:47 +0800

    Update the download script

--- models/download_production_model.sh
+++ models/download_production_model.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-wget http://172.16.15.108/DL/PVA-RC/v8_iter_50000.caffemodel -O  /root/pva-faster-rcnn/models/v8_iter_50000.caffemodel
+wget http://172.16.15.108/DL/PVA-RC/v8_iter_50000.caffemodel -O  /root/pvanet/models/v8_iter_50000.caffemodel
 
